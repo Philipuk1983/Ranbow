66613c7de652bb16d81fc0a4d49e6a8db7441e02
philip <phili@DESKTOP-1U6UQGA>
2017-02-15 15:34:15 +0000

    Initial Commit

---
+++ Ranbow.TestFixtures/TestClassFixture.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ranbow;
+
+namespace Ranbow.TestFixtures
+{
+    [TestFixture]
+    public class TestClassFixture
+    {
+        [Test]
+        public void Test_This()
+        {
+            var testClass = TestClass.GetInstance();
+            var addThis = 4;
+            var andThis = 7;
+            var expectedValue = 11;
+            var result = testClass.TestAdding(addThis, andThis);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+    }
+}
---
+++ Ranbow/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranbow
+{
+    public class TestClass
+    {
+        public static TestClass GetInstance()
+        {
+            return new TestClass();
+        }
+
+        protected TestClass()
+        {
+
+        }
+
+        public int TestAdding(int addThis, int andThis)
+        {
+            return addThis + andThis;
+        }
+    }
+}
